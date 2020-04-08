480e18bfbcec41c7b78ebf6556a2465b0e9036db
1000JI <chjh1992@gmail.com>
2020-04-08 18:58:57 +0900

    feat: Data Init

--- MyCalendar/GongNoRi/ColorSet.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ColorSet.swift
-//  GongNoRi
-//
-//  Created by 천지운 on 2019/10/31.
-//  Copyright © 2019 jwcheon. All rights reserved.
-//
-
-import Foundation
--- MyCalendar/GongNoRi/allMethod.swift
+++
@@ -1,9 +0,0 @@
-//
-//  allMethod.swift
-//  GongNoRi
-//
-//  Created by 천지운 on 2019/10/25.
-//  Copyright © 2019 jwcheon. All rights reserved.
-//
-
-import Foundation
